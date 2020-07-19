b4a53152b16b9eab99047098427f80cfbbd8a0a8
fariqodri <arfixfari@gmail.com>
2020-07-19 21:24:42 +0700

    create starters

---
+++ command.py
@@ -0,0 +1,22 @@
+from abc import abstractmethod, ABC
+from receiver import Receiver
+from typing import List, Any
+
+class Command(ABC):
+    @abstractmethod
+    def execute(self) -> None:
+        pass
+
+class FirstPrimesCommand(Command):
+    def __init__(self, n: int, receiver: Receiver):
+        pass
+
+    def execute(self):
+        return super().execute()
+
+class SortAndCombineCommand(Command):
+    def __init__(self, first: List[Any], second: List[Any], receiver: Receiver):
+        pass
+
+    def execute(self):
+        return super().execute()
